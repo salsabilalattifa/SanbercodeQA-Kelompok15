edc688473fe60d87e23ad4fb6b6ebd4dac7f02aa
aprilia <apriliawidya1112@gmail.com>
2023-01-19 15:41:54 +0700

    menambahkan negative case untuk login

--- login.py
+++ login.py
@@ -23,6 +23,34 @@ class Login_kelompok_15(unittest.TestCase):
         driver.find_element(By.NAME, "login").click()
         time.sleep(2)
 
+    def testLogin_wrong_password(self):
+        driver = self.driver
+        driver.get("https://itera-qa.azurewebsites.net/Login/Index/")  # buka websitenya
+        driver.maximize_window()
+        time.sleep(1)
+        # isi username
+        driver.find_element(By.ID, "Username").send_keys("admin")
+        time.sleep(1)
+        # isi Password
+        driver.find_element(By.ID, "Password").send_keys("123456")
+        time.sleep(1)
+        driver.find_element(By.NAME, "login").click()
+        time.sleep(2)
+
+    def testLogin_wrong_username(self):
+        driver = self.driver
+        driver.get("https://itera-qa.azurewebsites.net/Login/Index/")  # buka websitenya
+        driver.maximize_window()
+        time.sleep(1)
+        # isi username
+        driver.find_element(By.ID, "Username").send_keys("inisalah")
+        time.sleep(1)
+        # isi Password
+        driver.find_element(By.ID, "Password").send_keys("admin")
+        time.sleep(1)
+        driver.find_element(By.NAME, "login").click()
+        time.sleep(2)
+
     def testLogin_failed_blank(self):
         driver = self.driver
         driver.get("https://itera-qa.azurewebsites.net/Login/Index/")  # buka websitenya
